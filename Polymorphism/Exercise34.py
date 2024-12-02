7d360f8eef89cf80d9809fe5afa6420cc880c4fd
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-02 18:07:30 +0530

    first commit

---
+++ Polymorphism/Exercise34.py
@@ -0,0 +1,28 @@
+class Bird:
+    def intro(self):
+        print("There are different types of birds")
+
+    def flight(self):
+        print("most of the birds can fly but some cannot")    
+
+class parrot(Bird):
+    def flight(self):
+        print("Parrots can fly") 
+
+class penguin(Bird):
+    def flight(self):
+        print("Penguin do not fly")
+
+
+obj_bird = Bird()
+obj_parr = parrot()
+obj_peng = penguin()
+
+obj_bird.intro()
+obj_bird.flight()
+
+obj_parr.intro()
+obj_parr.flight()
+
+obj_peng.intro()
+obj_peng.flight()
