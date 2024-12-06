4367fb39f8c4dea618918f07e240aad29c41fd60
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-06 22:33:14 +0530

    updated commit

--- Polymorphism/type_hinting.py
+++ Polymorphism/type_hinting.py
@@ -1,2 +1,9 @@
-def myfunction(myparameter):
-    pass
+def myfunction(myparameter: int) -> str:
+    return f"{myparameter + 10}"
+
+def otherfunction(otherparameter: str):
+    print(otherparameter)
+
+otherfunction(myfunction(20))
+
+def dosth(param: list[int]):
