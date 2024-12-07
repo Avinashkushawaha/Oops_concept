14eba13b337de48bcaf094bcde941259972a7a5e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-07 15:17:57 +0530

    updated commit

--- Polymorphism/factory_design_pattern.py
+++ Polymorphism/factory_design_pattern.py
@@ -1,3 +1,37 @@
 from abc import ABCMeta, abstractstaticmethod
 
-class
+class IPerson(metaclass=ABCMeta):
+
+    @abstractstaticmethod
+    def person_method():
+        """ Interface Method """
+
+class Student(IPerson):
+    def __init__(self):
+        self.name = "Basic Student Name"
+
+    def person_mehtod(self):
+        print("I am a student")
+
+class Teacher(IPerson):
+
+    def __init__(self):
+        self.name = "Basic Teacher Name"
+
+    def person_method(self):
+        print("I am a teacher")
+
+class PersonFactory:
+    @staticmethod
+    def build_person(person_type):
+        if person_type == "Student":
+            return Student()
+        if person_type == "Teacher":
+            return Teacher()
+        print("Invalid Type")
+        return -1
+    
+if __name__ == "__main__":
+    choice = input("What type of perosn do you want to create?\n")
+    perosn = PersonFactory.build_person(choice)
+    person.person_method()    
