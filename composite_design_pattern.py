350f3df33f21a746b093da60a3b1fe68ba73af8c
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-08 22:37:58 +0530

    updated commit

--- composite_design_pattern.py
+++ composite_design_pattern.py
@@ -18,7 +18,37 @@ class Accounting(IDepartment):
     def print_department(self):
         print(f"Accounting Deparment: {self.employees}")
 
-class Accounting(IDepartment):
+class Development(IDepartment):
 
     def __init__(self, employees):
         self.employees = employees 
+
+    def print_department(self):
+        print(f"Developmetn Deparment: {self.employees}")
+
+class ParentDepartment(IDepartment):
+
+    def __init__(self, employees):
+        self.employees = employees    
+        self.base_employees = employees
+        self.sub_depts = []   
+
+    def add(self, dept):
+        self.sub_depts.append(dept) 
+        self.employees += dept.employees
+
+    def print_department(self):
+        print("Parent Department")
+        print(f"Parent Department Base Employees: {self.base_employees}")
+        for dept in self.sub_depts:
+            dept.print_department()
+        print(f"Total number of employees: {self.employees}") 
+
+dept1 = Accounting(200)
+dept2 = Development(170)
+
+parent_dept = ParentDepartment(30)
+parent_dept.add(dept1)
+parent_dept.add(dept2)
+
+parent_dept.print_department()
