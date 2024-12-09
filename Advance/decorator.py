8fb7c612782d7330291428796802c086cfab2582
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-09 19:49:52 +0530

    update commit

--- Advance/decorator.py
+++ Advance/decorator.py
@@ -5,5 +5,13 @@ def timer(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time() 
-        print(f"{} ran in {end-start} time")
+        print(f"{func.__name__} ran in {end-start} time")
+        return result
+    return wrapper
+
+@timer
+def example_function(n):
+    time.sleep(n)
+
+example_function(2)    
         
