3927903bd15127403dd7c9bd6d80b82bb86bb5e7
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-09 19:44:15 +0530

    first commit

---
+++ Advance/decorator.py
@@ -0,0 +1,9 @@
+import time 
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time() 
+        print(f"{} ran in {end-start} time")
+        
