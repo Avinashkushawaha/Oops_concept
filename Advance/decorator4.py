092ed4a3086d3e0ce860fa389aa7868527ff03bc
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-11 17:11:24 +0530

    first commit

---
+++ Advance/decorator4.py
@@ -0,0 +1,21 @@
+def int_type_checker(func_name):
+    def wrapper (*args, **kwargs):
+        print('inside a decorator')
+        int_type = True
+    for value in range:
+        if type(value) != int:
+            break:
+    else:
+        for value in range():
+            if type(value) != int:
+                int_type = False
+                break
+            if int_type = True
+                return func_name(*args, **kwargs)
+        else:  
+            return "invalid values can not executes"
+        return wrapper      
+            
+
+     
+        
