a782e47a7195e6323cdff8bdadf2ef39a38fa60e
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-11 16:47:53 +0530

    update commit

--- Advance/decorator3.py
+++ Advance/decorator3.py
@@ -2,5 +2,21 @@ def error_handler(func_name):
     def wrapper(*args, **kwargs):
         print(" inside a decorator ")
         try:
-         result = func_name(*args, **kwargs)
-         
+            result = func_name(*args, **kwargs)
+        except:
+          
+           return "some error occured"
+        else:
+           return result
+        
+    return wrapper
+    
+@error_handler
+def divide(n1: int, n2: int):
+    """function to divide two numbers."""
+    return n1 / n2
+
+print(divide(10, 2))  
+print(divide(10, 0))
+
+
