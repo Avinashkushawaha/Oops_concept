1ffe4346d95bae2cc5fba90598fc5e87c7debcb5
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-11 17:29:42 +0530

    update commit

--- Advance/decorator4.py
+++ Advance/decorator4.py
@@ -1,20 +1,26 @@
 def int_type_checker(func_name):
+
     def wrapper (*args, **kwargs):
         print('inside a decorator')
-        int_type = True
-    for value in range:
-        if type(value) != int:
-            break:
-    else:
-        for value in range():
-            if type(value) != int:
-                int_type = False
-                break
-            if int_type = True
-                return func_name(*args, **kwargs)
+        
+        int_type = all(isinstance(value, int) for value in  args)   
+
+        int_type &= all(isinstance(value, int) for value in kwargs.values()) 
+        
+        
+        if int_type:
+            return func_name(*args, **kwargs)
         else:  
             return "invalid values can not executes"
-        return wrapper      
+    return wrapper  
+
+@int_type_checker
+def add(n1:int, n2=int):
+    return n1 + n2
+print(add(10, 20))
+print(add(10, "20"))
+print(add(10, n2=30))
+print(add(10, n2="next"))      
             
 
      
