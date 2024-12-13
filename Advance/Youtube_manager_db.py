d9c2ce2070c226ff193d2d67c8a63d0316c3e2fd
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-13 21:59:30 +0530

    first commit

---
+++ Advance/Youtube_manager_db.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+print("\n Youtube manager app with DB")
+print("1. List Videos")
+print("2. Add Videos")
+print("3. Update Videos")
+print("4.Delete Videos")
+print("5. exit app")
+
+choice = input("Enter your choice")
+
+if __name__ == "__main__":
