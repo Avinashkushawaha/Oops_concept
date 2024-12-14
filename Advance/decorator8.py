1bb826b0556f16587e29b13fe3ee46b7c263be48
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-14 20:55:59 +0530

    updated commit

--- Advance/decorator8.py
+++ Advance/decorator8.py
@@ -3,3 +3,8 @@ def log_decorator(func):
         print(f"Function {func.__name__} called with {args} and {kwargs}")
         return func(*args, **kwargs)
     return wrapper
+@log_decorator
+def add(a, b):
+    return a + b
+
+print(add(2, 3))
