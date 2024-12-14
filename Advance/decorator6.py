39d9704bf0af5800fe93133e7b20eb7f14fbe828
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-14 20:52:50 +0530

    first commit

---
+++ Advance/decorator6.py
@@ -0,0 +1,5 @@
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print(f"Wrapper executed before {original_function.__name__}")
+        return original_function(*args, **kwargs)
+    return wrapper_function
