130ac46babc7db0f9441905979c7e61525188315
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-14 20:54:54 +0530

    updated commit

--- Advance/decorator7.py
+++ Advance/decorator7.py
@@ -3,3 +3,9 @@ def decorator_function(original_function):
         print(f"Wrapper executed before {original_function.__name__}")
         return original_function(*args, **kwargs)
     return wrapper_function
+
+@decorator_function
+def display():
+    print("Display function ran")
+
+display()
