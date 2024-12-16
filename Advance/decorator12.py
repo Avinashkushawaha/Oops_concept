453628addab876f0bd902ab778c87590613a5886
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-17 01:06:07 +0530

    updated commit

--- Advance/decorator12.py
+++ Advance/decorator12.py
@@ -22,5 +22,35 @@ def class_deco(cls_name):
         if callable(getattr(cls_name, attr)) and not attr.swithswith("__"):
             method = getattr(cls_name, attr)
             decorated_method = method_decoration(method)
-            
+            setattr(cls_name, attr, decorated_method)
+    return cls_name
+
+@class_deco
+class Mtah:
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+
+    def add(self):
+        return self.n1 + self.n2
+    
+    def divide(self):
+        if self.n2 != 0:
+            return self.n1 / self.n2
+        else:
+            return "Division by zero is not allowed."
+        
+        def multiply(self):
+            return self.n1 * self.n2
+        
+        
+math_obj = Math(10, 5)
+math_obj.add()
+math_obj.divide()
+math_obj.multiply()
+
+
+
+
+
                     
