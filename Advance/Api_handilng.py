edd16588193442f622c8301e66598cad6f4f476a
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-16 23:07:53 +0530

    updated commit

--- Advance/Api_handilng.py
+++ Advance/Api_handilng.py
@@ -1,7 +1,7 @@
 import requests
 
 def fectch_random_user_freeapi():
-    url = "https://api.freeapi.app/api/v1/public/randomproducts/product/random"
+    url = "https://api.freeapi.app/api/v1/public/randomproducts/product/random"  
     response = requests.get(url)
     data = response.json()
 
--- Advance/decorator11.py
+++ Advance/decorator11.py
@@ -1,14 +1,26 @@
-class polygon (ABC):
+from abc import ABC, abstractmethod
+class Polygon (ABC):
     @abstractmethod
     def roofsides (self):
         pass
-class Triangles (polygon):
+class Triangle (Polygon):
     # overriding abstract method
     def roofsides(self):
         print("I have 3 sides")
-class pentagon(polygon):
+class Pentagon(Polygon):
     def roofsides(self):
-        print("I have 5 sides"):
-class hexagon (polygon):
+        print("I have 5 sides")
+class Hexagon (Polygon):
     def roofsides(self):
         print("I have 6 sides")                
+
+
+
+triangle = Triangle()
+triangle.roofsides()  
+
+pentagon = Pentagon()
+pentagon.roofsides()  
+
+hexagon = Hexagon()
+hexagon.roofsides() 
