f6c4c409da2dc95eaedf0b65620a11b68e7ef128
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-17 00:59:40 +0530

    updated commit

--- Advance/decorator12.py
+++ Advance/decorator12.py
@@ -9,5 +9,18 @@ class Math:
     def multiply(self):
         return self.n1 * self.n2
 
-def class_deco (cls_name):
-    def       
+def class_deco(cls_name):
+    def method_decoration(method_name):
+        def wrapper(*args,**kwargs):
+            print(f'Executing{method_name.__name__}')
+            result = method_name(*args, **kwargs)
+            print(f'Result: {result}')
+            return result
+        return wrapper
+    
+    for attr in dir(cls_name):
+        if callable(getattr(cls_name, attr)) and not attr.swithswith("__"):
+            method = getattr(cls_name, attr)
+            decorated_method = method_decoration(method)
+            
+                    
