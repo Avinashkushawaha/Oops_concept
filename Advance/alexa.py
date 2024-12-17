5a43a9a24c09f5eb95c0a25db3558416a99cbe80
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-17 16:15:51 +0530

    updated commit

--- Advance/alexa.py
+++ Advance/alexa.py
@@ -33,3 +33,45 @@ def take_command():
             print("Sorry I couldn't hear anything.")
         return command
 
+def run_alexa():
+    command = take_command()
+    print(command)
+
+
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+
+
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+
+
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        try:
+            info = wikipedia.summary(person, 1)
+            print(info)
+            talk(info)
+        except wikipedia.exceptions.DisambiguationError as e:
+            talk("There are multiple results for this person , can you be more specific?")
+        except wikipedia.exceptions.HTTPTimeoutError:
+            talk("Sorry, I couldn't retrieve information at the moment.")        
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+
+    elif 'are you single' in command:
+        talk('I am in a relation with wifi')
+
+    elif 'joke' in command:
+        joke = pyjokes.get_jokes()
+        talk(joke)
+    else:
+        talk('Please say the command again.')
+
+while True:
+    run_alexa()                                           
+
+    
