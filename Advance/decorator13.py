42fcc9baf045bbd1987dec7c5cf6819b17442ffa
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-18 19:50:44 +0530

    updated commit

--- Advance/decorator13.py
+++ Advance/decorator13.py
@@ -1,4 +1,31 @@
 from time import ctime
 def delay_invoked_time(func):
     def wrapper(*args, **kwargs):
-        print(f"{func name} invoked at {ctime()}")
+        print(f"{func.__name__} invoked at {ctime()}")
+        return func(*args, **kwargs)
+    return wrapper
+
+def Math_deco(cls):
+    for name, method in cls.__dict__.items():
+        if callable(method):
+
+            decorated_method = delay_invoked_time(method)
+            setattr(cls, name, decorated_method)
+    return cls
+
+@Math_deco
+class WhatsDown:
+    def change_dp(self):
+        print('changing dp') 
+
+    def upload_state(self):
+        print("uploading status")
+
+    def react (self):
+        print('reacting to msg')
+
+whats_app = WhatsDown()
+whats_app.change_dp()
+whats_app.upload_state()
+whats_app.react()                        
+
