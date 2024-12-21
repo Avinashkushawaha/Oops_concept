6e3b143c5b5cbb5421bdb31813a5b2608f56c3e9
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-22 02:04:15 +0530

    updated commit

--- Advance/Exercise.py
+++ Advance/Exercise.py
@@ -1,12 +1,21 @@
 import timeit
 from functools import cache
 
+def fibonacci_no_cache(n):
+    if n <= 2:
+        return n
+    return fibonacci_no_cache(n - 1) + fibonacci_no_cache(n - 2)
 
+@cache
+def fibonacci_cache(n):
+    if n <= 2:
+        return n
+    return fibonacci_cache(n - 1) + fibonacci_cache(n - 2)
 
 def measure_time():
     n = 40
     no_cache_time = timeit.timeit(lambda: fibonacci_no_cache(n), number=1)
-    cache_time = timeit.timeit(lambda: fibonacci(n), number=1)
+    cache_time = timeit.timeit(lambda: fibonacci_cache(n), number=1)
 
     print(f"Execution time without cache: {no_cache_time:4f} seconds")
     print(f"Execution time with cache: {cache_time:10f} seconds")
