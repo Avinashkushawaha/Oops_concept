5b51bc0b5b2d0129e5bdf785703b13cef05ded47
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-12-25 00:49:25 +0530

    updated commit

--- Advance/iterators.py
+++ Advance/iterators.py
@@ -2,4 +2,10 @@ def gen(n):
     for i in range(n):
         yield i
 
-ob1 = gen(1000        
+ob1 = gen(1000 )
+print(next(ob1)) 
+print(next(ob1))   
+print(next(ob1))  
+print(next(ob1)) 
+print(next(ob1))
+print(next(ob1))    
